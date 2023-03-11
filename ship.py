76db9bfad84f66bfaa7ba8a46eb72abbc810ce16
Carlos Casado Vargas <carloscasadovargas@gmail.com>
2023-03-11 13:16:43 -0500

    Limit ship's range

--- ship.py
+++ ship.py
@@ -8,8 +8,8 @@ class Ship:
         """Initialize user's ship and set its starting position"""
 
         # Movement flags
-        self.moving_right = False
         self.moving_left = False
+        self.moving_right = False
 
         self.screen = screen
         self.game_settings = game_settings
@@ -35,10 +35,10 @@ class Ship:
     def update(self):
         """Update ship's position based on the movement flag"""
 
-        if self.moving_right:
-            self.center += self.game_settings.ship_speed_factor
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.center -= self.game_settings.ship_speed_factor
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.game_settings.ship_speed_factor
 
         # Update rect object from self.center
         self.rect.centerx = self.center
